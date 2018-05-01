636ff17802d32482f4e222a07e517a8d8525cddf
Thanh Doi <thanh.doikhac@gmail.com>
2018-05-01 09:51:07 +0700

    init RESTful API

--- app/User.php
+++ app/User.php
@@ -26,4 +26,11 @@ class User extends \TCG\Voyager\Models\User
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function generateToken() {
+        $this->api_token = str_random(60);
+        $this->save();
+
+        return $this->api_token;
+    }
 }
